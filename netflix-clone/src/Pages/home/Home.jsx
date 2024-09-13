176994d2c087cccf05b76c61591b46c836482c15
BMTUMEBO <berhonest@gmail.com>
2024-09-13 17:21:48 -0400

    axios and request

--- netflix-clone/src/Pages/home/Home.jsx
+++ netflix-clone/src/Pages/home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
+import Banner from '../../components/banner/Banner'
+
 
 function Home() {
   return (
     <div>
        <Header/>
+       <Banner/>
        <Footer/>
+       
     </div>
   )
 }
